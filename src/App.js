fc58c921f88c75234fd8e90f5d60f06e8f483fe4
Madan elriano <fillar72@gmail.com>
2026-01-28 17:20:20 +0700

    Create src/App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Video Editor</h1>
+      </header>
+      <main>
+        <p>Your modern and professional video editing experience starts here!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
